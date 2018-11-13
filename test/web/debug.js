5cd0d079adac0ee3e3b1fc258e170fedfe8b910a
Busheng Xi <busheng.xi@yvw.com.au>
2018-11-13 14:08:14 +1100

    Updated name

--- test/web/debug.js
+++ test/web/debug.js
@@ -21,10 +21,10 @@ function f() {
 
             console.warn( 0, text );
 
-            console.warn( 11, fache.storage.requestResponsePairs[0].response );
-            // console.warn( 12, fache.storage.requestResponsePairs[0].response.text() );
+            console.warn( 11, fache.storage.cachedPairs[0].response );
+            // console.warn( 12, fache.storage.cachedPairs[0].response.text() );
 
-            let clone = fache.storage.requestResponsePairs[0].response.clone();
+            let clone = fache.storage.cachedPairs[0].response.clone();
             console.warn( 13, clone );
             let cloneOfClone = clone.clone();
             console.warn( 14, clone.text() );
